57e5f1c5f73e78daf7c8f8ad16dd86b8eb8153e6
Fauzan Priatmana <fauzanpriatmanas@gmail.com>
2026-02-01 22:00:23 +0700

    Create Footer Components

---
+++ src/components/footer.tsx
@@ -0,0 +1,103 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="mx-auto border-t border-neutral-200 px-16 py-16">
+      <div className="flex flex-col">
+        <div className="flex flex-col justify-between gap-16">
+          {/* Footer Content */}
+          <div>
+            <div className="flex flex-wrap justify-between gap-10">
+              <div className="flex flex-col gap-4">
+                <div className="animate-bounce">
+                  <a className="text-xl font-bold" href="">
+                    TamaFawx
+                  </a>
+                </div>
+                <div>
+                  <p className="text-sm opacity-60">
+                    &copy; 2026{" "}
+                    <span className="font-semibold underline">TamaFawx</span>.
+                    All rights reserved.
+                  </p>
+                  <p className="text-sm opacity-60">
+                    Developed and create by{" "}
+                    <span className="font-semibold underline">
+                      Fawzan Priatmana
+                    </span>
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex flex-wrap gap-16 xl:gap-16">
+                <section>
+                  <h2 className="text-base font-semibold">Resources</h2>
+                  <ul className="mt-3 flex flex-col gap-2">
+                    <li className="opacity-60 transition duration-250 ease-in-out hover:opacity-100">
+                      <Link href={"/"}>Home</Link>
+                    </li>
+                    <li className="opacity-60 transition duration-250 ease-in-out hover:opacity-100">
+                      <Link href={"/about"}>About</Link>
+                    </li>
+                    <li className="opacity-60 transition duration-250 ease-in-out hover:opacity-100">
+                      ToS
+                    </li>
+                    <li className="opacity-60 transition duration-250 ease-in-out hover:opacity-100">
+                      FAQ
+                    </li>
+                  </ul>
+                </section>
+                <section>
+                  <h2 className="text-base font-semibold">Find Me</h2>
+                  <ul className="mt-3 flex flex-col gap-2 opacity-70">
+                    <li className="opacity-60 transition duration-250 ease-in-out hover:opacity-100">
+                      <Link href={"https://x.com/tamafawx"} target="_blank">
+                        X (Formerly Twitter)
+                      </Link>
+                    </li>
+                    <li className="opacity-60 transition duration-250 ease-in-out hover:opacity-100">
+                      <Link
+                        href={"https://www.facebook.com/tamafawx/"}
+                        target="_blank"
+                      >
+                        Facebook
+                      </Link>
+                    </li>
+                    <li className="opacity-60 transition duration-250 ease-in-out hover:opacity-100">
+                      <Link href={"https://t.me/tamafawx"} target="_blank">
+                        Telegram
+                      </Link>
+                    </li>
+                    <li className="opacity-60 transition duration-250 ease-in-out hover:opacity-100">
+                      <Link
+                        href={"https://www.instagram.com/tamafawx/"}
+                        target="_blank"
+                      >
+                        Instagram
+                      </Link>
+                    </li>
+                    <li className="opacity-60 transition duration-250 ease-in-out hover:opacity-100">
+                      <Link
+                        href={"https://bsky.app/profile/tamafawx.bsky.social"}
+                        target="_blank"
+                      >
+                        Bluesky
+                      </Link>
+                    </li>
+                    <li className="opacity-60 transition duration-250 ease-in-out hover:opacity-100">
+                      <Link href={"/"}>Find more</Link>
+                    </li>
+                  </ul>
+                </section>
+              </div>
+            </div>
+          </div>
+          <p className="flex justify-center text-center opacity-60">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            posuere lectus eu ex egestas, sed.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
