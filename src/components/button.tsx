2eb54167ecdf87d109c17fe9a57383af13efb2a5
Fauzan Priatmana <fauzanpriatmanas@gmail.com>
2026-03-25 16:50:08 +0700

    refactor button component: move Link wrapper outside of the button div for improved structure

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -9,15 +9,15 @@ interface ButtonProps {
 
 export default function Button(props: ButtonProps) {
   return (
-    <div className="flex h-10 w-25 items-center justify-center rounded-sm bg-[#f3b77a] text-white opacity-75 transition duration-250 ease-in-out hover:opacity-100">
-      <Link
-        className="font-bold"
-        title={props.title}
-        href={props.link || "/"}
-        scroll={props.scroll}
-      >
+    <Link
+      className="font-bold"
+      title={props.title}
+      href={props.link || "/"}
+      scroll={props.scroll}
+    >
+      <div className="flex h-10 w-25 items-center justify-center rounded-sm bg-[#f3b77a] text-white opacity-75 transition duration-250 ease-in-out hover:opacity-100">
         {props.buttonName}
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
